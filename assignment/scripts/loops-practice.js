da03171868a0d75f89578851977a79383725a5d4
Mahamud Aden <mahamudadn@gmail.com>
2023-04-12 16:25:18 -0500

    added while loop to stars array

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -42,15 +42,23 @@ console.log(i)
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
+
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
+for (world of stars) {
+  console.log('loop of stars', world)
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+while(stars) {
+  console.log(stars)
+  stars++
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
