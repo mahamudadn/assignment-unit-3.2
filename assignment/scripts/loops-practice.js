1d9f9b752a6dd1a76b9b73b8b873b1ef47ba434a
Mahamud Aden <mahamudadn@gmail.com>
2023-04-25 00:47:05 -0500

    logged out each star

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -58,9 +58,11 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
-    while(stars) {
-      console.log(stars)
-      stars++
+      i = 0;
+      
+    while ( i < stars.length) {
+      console.log('i have logged out each star in the array' , stars[i])
+      i++;
     }
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
